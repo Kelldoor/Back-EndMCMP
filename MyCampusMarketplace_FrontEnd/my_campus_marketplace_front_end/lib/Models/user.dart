1b33acd6c850af960364065992789b91cd131fc4
dylanhallila <dylan.hallila@my.sctcc.edu>
2024-04-09 21:17:10 -0500

    Updated User class

--- MyCampusMarketplace_FrontEnd/my_campus_marketplace_front_end/lib/Models/user.dart
+++ MyCampusMarketplace_FrontEnd/my_campus_marketplace_front_end/lib/Models/user.dart
@@ -1,19 +1,24 @@
 class User {
+  int userID;
   String firstName;
   String lastName;
   String studentID;
   String studentEmail;
   String userName;
-  String passwordHash;
+  bool admin;
+  bool banned;
+  DateTime creationDate;
 
-  User({
-    required this.firstName,
-    required this.lastName,
-    required this.studentID,
-    required this.studentEmail,
-    required this.userName,
-    required this.passwordHash,
-  });
+  User(
+      {required this.userID,
+      required this.firstName,
+      required this.lastName,
+      required this.studentID,
+      required this.studentEmail,
+      required this.userName,
+      required this.admin,
+      required this.banned,
+      required this.creationDate});
 
   String getFullName() {
     return "$firstName $lastName";
